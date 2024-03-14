2eefa96a84fc554d50eab145bfad86c9a2380a8d
akimerin <146888622+akimerin@users.noreply.github.com>
2024-03-14 21:48:09 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,21 +5,28 @@
 
 using namespace std;
 
-#define V 6  //  СѓРєР°Р·Р°С‚СЊ РєРѕР» РІРѕ РІРµСЂС€РёРЅ РІ РіСЂР°С„Рµ
+#define V 6  //  указать кол во вершин в графе
 
 bool bfs(int rGraph[V][V], int s, int t, int parent[])
 {
     bool visited[V];
+
+
+
     memset(visited, 0, sizeof(visited));
+
     queue<int> q;
     q.push(s);
+
     visited[s] = true;
     parent[s] = -1;
 
     while (!q.empty())
     {
         int u = q.front();
+
         q.pop();
+
         for (int v = 0; v < V; v++)
         {
             if (!visited[v] && rGraph[u][v] > 0)
@@ -30,16 +37,20 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
             }
         }
     }
+
     return (visited[t] == true);
 }
 
 int fordFulkerson(int graph[V][V], int s, int t)
 {
     int u, v;
-    int rGraph[V][V];  // РћСЃС‚Р°С‚РѕС‡РЅР°СЏ СЃРµС‚СЊ
+    int rGraph[V][V];  // Остаточная сеть
     for (u = 0; u < V; u++)
         for (v = 0; v < V; v++)
+
+
             rGraph[u][v] = graph[u][v];
+
     int parent[V];
     int max_flow = 0;
 
@@ -56,40 +67,48 @@ int fordFulkerson(int graph[V][V], int s, int t)
         {
             u = parent[v];
             rGraph[u][v] -= path_flow;
+
             rGraph[v][u] += path_flow;
         }
 
         max_flow += path_flow;
 
-        // Р’С‹РІРѕРґ СЃРѕСЃС‚РѕСЏРЅРёСЏ РіСЂР°С„Р°
-        cout << "the state of the graph after increasing the flow by " << path_flow << ":" << endl;
+        // Вывод состояния графа
+        cout << "Состояние графа после увеличения потока на " << path_flow << ":" << endl;
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
+
                 cout << rGraph[i][j] << " ";
             }
             cout << endl;
         }
         cout << endl;
     }
+
     return max_flow;
 }
+
 int main()
 {
-    
 
+
+    setlocale(LC_ALL, "RUS");
     int graph[V][V];
-    cout << "Enter the bandwidth matrix for the graph (" << V << "x" << V << "):" << endl;
+    cout << "Введите матрицу пропускных способностей для графа (" << V << "x" << V << "):" << endl;
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
+
+
             cin >> graph[i][j];
+
         }
     }
     int source, sink;
-    cout << "Enter the source and drain: ";
+    cout << "Введите исток и сток: ";
     cin >> source >> sink;
 
-    // Р’С‹Р·РѕРІ Р°Р»РіРѕСЂРёС‚РјР° Р¤-Р¤
-    cout << "The maximum flow in the graph: " << fordFulkerson(graph, source, sink) << endl;
+    // Вызов алгоритма Ф-Ф
+    cout << "Максимальный поток в графе: " << fordFulkerson(graph, source, sink) << endl;
 
     return 0;
 }
